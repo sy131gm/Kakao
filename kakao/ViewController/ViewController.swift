663ace4de430355dc35242d858699ecc694423bf
SEYEON AN <sy131gm@gmail.com>
2020-08-09 16:41:04 +0900

    first-pull-request

--- kakao/ViewController/ViewController.swift
+++ kakao/ViewController/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  kakao
-//
-//  Created by Seyeon An on 2020/07/26.
-//  Copyright © 2020 Seyeon An. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
